80074c20ea6d99b67189b5acf63c610b07039471
Kyytox <thomas.caillault@protonmail.com>
2023-07-02 02:36:39 +0200

    remove loop on df in transform data process

--- airflow/plugins/operators/transform_data.py
+++ airflow/plugins/operators/transform_data.py
@@ -166,33 +166,14 @@ def transform_data():
     df.rename(columns={'text_tweet': 'text_formatted_tweet'}, inplace=True)
     df["text_formatted_tweet"] = df["text_formatted_tweet"].apply(format_text)
 
+    # detect sentiment
+    df_sentiment = df.apply(detect_sentiment, axis=1, args=(tokenizer, config, model))
 
+    # reindex df
+    df = pd.merge(df, df_sentiment, on="id_tweet")
 
-    # loop df 2000 by 2000 and send to AWS S3 
-    # to avoid memory error
-    for i in range(0, len(df), 2000):
-        df_part = df.iloc[i:i+2000]
-
-        # detect sentiment
-        df_sentiment = df_part.apply(detect_sentiment, axis=1, args=(tokenizer, config, model))
-
-        # reindex df
-        df_part = pd.merge(df_part, df_sentiment, on="id_tweet")
-
-        # send to AWS S3
-        send_to_aws_partition(df_part, "Gold/tweets_transform.parquet")
-
-
-    # # loop df 2000 by 2000
-    # for i in range(0, len(df), 2000):
-    #     # detect sentiment
-    #     df_sentiment = df.apply(detect_sentiment, axis=1, args=(tokenizer, config, model))
-
-    #     # reindex df
-    #     df = pd.merge(df, df_sentiment, on="id_tweet")
-
-    #     # Send to AWS S3
-    #     send_to_aws_partition(df, "Gold/tweets_transform.parquet")
+    # Send to AWS S3
+    send_to_aws_partition(df, "Gold/tweets_transform.parquet")
 
 
 
